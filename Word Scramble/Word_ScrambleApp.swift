ebc6a3274ffe78d9fd9aafc4942fff240172525a
Ananya Elizabeth George <50709725+ananya190@users.noreply.github.com>
2021-10-15 00:35:50 +0530

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Ananya George on 10/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
